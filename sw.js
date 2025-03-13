906bfe8a916a72628886cd9dec1b725861845edf
VOLTAGE_LORD <officialvoltinstasnap@gmail.com>
2025-03-13 06:01:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open("volt-audio-cache").then((cache) => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/manifest.json",
+                "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+            ]);
+        })
+    );
+});
 
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
